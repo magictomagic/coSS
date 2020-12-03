c83c7c5c2422cf2595778c12cf3cea338f2664b7
magictomagic <magictomagic@z1172765646@gmail.com>
2020-12-04 02:56:59 +0800

    moved

---
+++ coSS_v4/v4_1/coSS_v3.py
@@ -0,0 +1,42 @@
+'''
+Author: magictomagic
+Date: 2020-11-19 21:34:32
+LastEditors: magictomagic
+LastEditTime: 2020-11-24 17:17:45
+Description: file content
+'''
+
+# pyinstaller -F .\coSS_v3.py --noconsole
+
+import win32clipboard as w
+import keyboard
+import transEntoCh
+import time
+import win32api
+import win32con
+import readFromJson
+import feedback
+
+def getText():
+    w.OpenClipboard()
+    d = w.GetClipboardData(win32con.CF_TEXT)
+    w.CloseClipboard()
+    return(d).decode('GBK')
+
+
+def copyGfCSA():
+    while keyboard.is_pressed("alt") or keyboard.is_pressed("l"):
+        time.sleep(0.1)
+    win32api.keybd_event(17,0,0,0)  #ctrl
+    win32api.keybd_event(67,0,0,0)  #c
+    win32api.keybd_event(67,0,win32con.KEYEVENTF_KEYUP,0) #释放按键
+    win32api.keybd_event(17,0,win32con.KEYEVENTF_KEYUP,0)
+    time.sleep(0.1)
+    processedText = getText().strip()
+    readFromJson.saveSentence(processedText, transEntoCh.translate(processedText))
+    feedback.toastWinInfo("asd.ico", 10, True)  # TODO: custom your feedback here
+
+
+if __name__=="__main__":
+    keyboard.add_hotkey('ctrl+alt+l', copyGfCSA, [], False)
+    keyboard.wait('esc')
---
+++ d2.sql
@@ -0,0 +1,3 @@
+use coSS1;
+create table `oSSd1`(`原文` varchar(100), 
+					`译文` varchar(100))
